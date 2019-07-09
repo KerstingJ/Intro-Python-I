ad21c45943761f06b3ee76e4ce78cb3d49ec337b
Josh <Kersting.Josh@Gmail.com>
2019-07-08 21:19:27 -0400

    finished up to IO assignment

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,10 +9,26 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+import os
+# get path up to current file
+cwd = os.path.realpath(__file__)
+path = cwd.replace("13_file_io.py", "foo.txt")
+
+with open(path) as foo:
+    for line in foo:
+        print(line, end="")
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+cwd = os.path.realpath(__file__)
+path = cwd.replace("13_file_io.py", "bar.txt")
+
+bar = open(path, mode="w")
+for i in range(3):
+    bar.write(f"This is line: {i}\n")
+
+bar.close()
